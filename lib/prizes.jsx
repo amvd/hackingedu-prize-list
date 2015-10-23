0445df45d08d4bf2a629eae30605cc06bfbf044e
_nothing <adeasis10@gmail.com>
2015-10-22 23:19:08 -0700

    add base files and json object to lib

---
+++ lib/prizes.jsx
@@ -0,0 +1,199 @@
+var prizes = [
+  {
+    "title": "HackingEDU EDTech Champions",
+    "prizes": [
+      "500 Startups Interview",
+      "ATT Aspire Accelerator in person interview",
+      "Oculus DK2s",
+      "$5000 scholarship from Draper University (per applicant accepted)",
+      "Hackster: Goodie Bag: Microcontroller, T-shirt, Stickers",
+      "Mentorship from NFL"
+    ],
+    "url": ""
+  },
+  {
+    "title": "HackingEDU 2nd Place Winner",
+    "prizes": [
+      "Interview and dinner with GSV Labs Staff ",
+      "Interview with Learn Capital",
+      "BT Controller & Speakers",
+      "Emotive Headset",
+      "Roblox Swag bag: ROBLOX Swag Bag: Beanie, Water Bottle, T-Shirt",
+      "Draper: Teams 3 & 2 receive $3,500 off tuition(per applicant accepted)",
+      "Mentorship from NFL"
+    ],
+    "url": ""
+  },
+  {
+    "title": "HackingEDU 3rd Place Runner up Prize",
+    "prizes": [
+      "Interview with Draper University",
+      "Dinner with MakeSchool Founders",
+      "Particle Boards",
+      "Muse Headset",
+      "Galvanize: Free desk space, diversity scholarship prize",
+      "HTC Phone",
+      "Draper: Teams 3 & 2 receive $3,500 off tuition(per applicant accepted)",
+      "Mentorship from NFL",
+    ],
+    "url": ""
+  },
+  {
+    "title": "Best use of Chegg API",
+    "prizes": [
+      "Up to $5000 of Chegg Services in total, which includes:",
+      "1 Year free Chegg Textbooks",
+      "1 Year free Chegg Study (Textbook Solutions)",
+      "4 hours of tutoring with Chegg Tutors for each team member"
+    ],
+    "url": ""
+  },
+  {
+    "title": "Best use to the Wolfram Technologies",
+    "prizes": [
+      "TRIP TO NYC for top team to Present Hack at Ignite Disrupt Edtech Conference",
+      "One year of Wolfram Programming Cloud Developer Level ",
+      "One year of Wolfram Mathematica Online ",
+      "One year of Wolfram|Alpha Pro for everyone on those teams"
+    ],
+    "url": "http://products.wolframalpha.com/api/"
+  },
+  {
+    "title": "#SocialGoodHack KPCB",
+    "prizes": [
+      "Finalist Position for KPCB Fellowship Position"
+    ],
+    "url": ""
+  },
+  {
+    "title": "Best Use of IBM Bluemix",
+    "prizes": [
+      "$4000"
+    ],
+    "url": "https://www.ng.bluemix.net/docs/"
+  },
+  {
+    "title": "Best Use of Target API",
+    "prizes": [
+      "$4000"
+    ],
+    "url": "https://dev.target.com/"
+  },
+  {
+    "title": "NameCheap Best Domain Main Prize",
+    "prizes": [
+      "$150 of Namecheap credits"
+    ],
+    "info": "Creativity & authenticity of domain name"
+    "url": "https://www.namecheap.com/support/api/intro.aspx"
+  },
+  {
+    "title": "Coolest Pebble Hack! (2)",
+    "prizes": [
+      "Pebble Times for top 2 teams"
+    ],
+    "url": "http://developer.getpebble.com/"
+  },
+  {
+    "title": "#bestuseofMYO",
+    "prizes": [
+      "4 Myos for Top team"
+    ],
+    "url": ""
+  },
+  {
+    "title": "Best Use of Dolby",
+    "prizes": [
+      "Sennheiser Game One headsets (for each team member)"
+    ],
+    "info": "Usage of our technology (based on usefulness and creativity)"
+    "url": "http://developer.dolby.com/"
+  },
+  {
+    "title": "GlobalCommit: Social Good Award",
+    "prizes": [
+      "$100 gift cards (google play, itunes)"
+    ],
+    "url": ""
+  },
+  {
+    "title": "Automattic Social Good Prize",
+    "prizes": [
+      "Apple Watches for the Team"
+    ],
+    "url": "https://automattic.com/code/widgets/api/"
+  },
+  {
+    "title": "Best use of Clusterpoint",
+    "prizes": [
+      "$5000"
+    ],
+    "info": "Criteria for challenge:\n1) sign-up for Cloud Account and enter promo code\n2) use Clusterpoint as back-end solution for your app\n3) relative degree of Clusterpoint utilization",
+    "url": "http://docs.clusterpoint.com/wiki/Main_Page"
+  },
+  {
+    "title": "Rubicon:Bring your Javascript, app development and big data experience and automate advertising",
+    "prizes": [
+      "First Prize: $1,000 Amazon Gift Card+Interview with Rubicon Project ",
+      "Second Prize: $500 Amazon Gift Card+Interview with Rubicon Project", 
+      "Third Prize (2 count): $250 Amazon Gift Card each + Interviews with Rubicon Project"
+    ],
+    "url": "http://dev.rubiconproject.com/"
+  },
+  {
+    "title": "Best use of Braintree API",
+    "prizes": [
+      "Keyboards for members of the team"
+    ],
+    "url": ""
+  },
+  {
+    "title": "Design the Future of WikiHow",
+    "prizes": [
+      "Hosted lunch with the CEO of WikiHow"
+    ],
+    "url": ""
+  },
+  {
+    "title": "Best Use of Linode",
+    "prizes": [
+      "Raspberry Pi 2s for each team member"
+    ],
+    "url": ""
+  },,
+  {
+    "title": "Best Leap Motion Hack (2)",
+    "prizes": [
+      "Leap Motion for everyone on the top 2 Teams"
+    ],
+    "url": ""
+  },
+  {
+    "title": "Best Use of Twist",
+    "prizes": [
+      "$500"
+    ],
+    "url": ""
+  },
+  {
+    "title": "Best use of Twilio API",
+    "prizes": [
+      "Wearable sleeping bag suit"
+    ],
+    "url": ""
+  },
+  {
+    "title": "Best Collaboration Hack using Moxtra SDK/API",
+    "prizes": [
+      "$1000"
+    ],
+    "url": ""
+  },
+  {
+    "title": "Best Distracted Driver App",
+    "prizes": [
+      "$50 Gift Cards"
+    ],
+    "url": ""
+  }
+]
